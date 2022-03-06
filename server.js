47c37aff957f0899317be2b16daf9bc5c96bc8f2
汪瀚堃 <wanghankun@wanghankundeMacBook-Air.local>
2022-03-06 12:31:08 -0800

    commited

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL || 
+  "postgres://postgres:root@localhost/cmpt276"
+  // ssl: {
+  //   rejectUnauthorized: false
+  // }
+});
+
 const app = express()
   app.use(express.urlencoded({ extended: false }))
   app.use(express.static(path.join(__dirname, 'public')))
