2c3eca8b983b7f785854ddb18930369421283904
Jiale Huang <jiale_huang@sfu.ca>
2022-03-06 19:20:46 -0800

    implement register

--- server.js
+++ server.js
@@ -19,11 +19,37 @@ const app = express()
   app.get('/', (req, res) => res.render('pages/index'))
   app.get('/login', (req, res) => res.render('pages/login'))
   app.get('/register', (req, res) => res.render('pages/register'))
+  app.get('/dashboard', (req, res) => res.render('pages/dashboard'))
   app.post('/login' , (req, res) => {
     res.send("hello");
 
   })
-  app.post('/register' , (req, res) => {
-    
+  app.post('/register' , async (req, res) => {
+    const userdata = req.body
+
+    if(!userdata.name)
+    {
+      return res.status(400).send('missing name')
+    }
+    if(!userdata.Useraccount)
+    {
+      return res.status(400).send('missing Useraccount')
+    }
+    if(!userdata.password)
+    {
+      return res.status(400).send('missing password')
+    }
+    if(!userdata.Confirmpassword)
+    {
+      return res.status(400).send('missing Confirmpassword')
+    }
+    //control password length?
+    if(userdata.password != userdata.Confirmpassword)
+    {
+      return res.status(400).send('different passwords') //400？
+    }
+    else{ //checking if there is same account
+      
+    }
   })
   app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
